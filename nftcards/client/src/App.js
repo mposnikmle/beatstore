fed66b18a50a3550e347670bf999c2a832662b43
Maximilian <mposnikmle@gmail.com>
2023-12-04 23:58:37 -0500

    testing git

---
+++ nftcards/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BeatCard from './components/card-component/BeatCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <BeatCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ nftcards/client/src/components/card-component/BeatCard.jsx
@@ -0,0 +1,8 @@
+function BeatCard(props) {
+    return (
+        <><h1>Hello from BeatCard</h1></>
+    );
+}
+
+
+export default BeatCard;
